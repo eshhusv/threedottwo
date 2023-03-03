3b5d9ec4858ee2abd7d9b8b9cb888f8024469d00
eshhusv <ak7527203@gmail.com>
2023-03-03 19:52:34 +0200

    Add project files.

---
+++ threedottwo/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int answer = 0;
+
+int i = 1;
+do
+{
+    if (num % i == 0) answer = i;
+    i++;
+} while (i < num);
+
+Console.Write(answer);
